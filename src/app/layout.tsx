1ed0ca1e6196ca0899b4dca463a5950fd267b468
초짜맨 <yongseung.song@icloud.com>
2024-06-11 15:58:16 +0900

    chores : basic settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,20 @@
-import type { Metadata } from 'next';
-import './globals.css';
+import type { Metadata } from "next"
+import "./globals.css"
 
+//cSpell:ignore Chozzaman's
 export const metadata: Metadata = {
   title: "Chozzaman's blog",
-  description: 'Hi! Thank you for visiting my blog',
-};
+  description: "Hi! Thank you for visiting my blog",
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
