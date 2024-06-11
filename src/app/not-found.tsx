0bbb4791d183752bd6d234d3ea6130f6d8e5b886
초짜맨 <yongseung.song@icloud.com>
2024-06-12 01:38:55 +0900

    README

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 function NotFound() {
   return (
     <div className="my-20 rounded-xl border-2 border-black px-10 py-20">
-      Woops! the page you're looking for does not seem to exist.
+      Oops! The page you're looking for does not seem to exist.
     </div>
   )
 }
